b8b195f3ddb25d278c3363d39851df888b82d0ac
Vinz Daniel Flores <floresvinz03@gmail.com>
2025-11-26 16:53:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
+
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+   body.classList.add('dark');
+}
+
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
+   body.classList.toggle('dark');
+
+   if (body.classList.contains('dark')) {
+      localStorage.setItem('theme', 'dark');
+   } else {
+      localStorage.setItem('theme', 'light');
+   }
 });
